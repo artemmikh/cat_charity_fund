9e5c93dcda38f87af31881b0875c15e12b5d2405
artemmikh <artem.mikh100@gmail.com>
2024-05-16 12:37:13 +0300

    Доработка инвестиций при создании проекта

--- app/services/investment.py
+++ app/services/investment.py
@@ -48,36 +48,38 @@ async def investing_new_donation(
 async def investing_to_new_project(
         project: CharityProjectDB,
         session: AsyncSession, ):
-    donation = await charityproject_crud.get_oldest_open_donation(session)
-    if donation is None:
-        return project
+    need_donation = project.full_amount
 
-    remaining_donation_amount = donation.full_amount
-    invested_amount = 0
+    while need_donation > 0:
 
-    while remaining_donation_amount > 0:
-        if project.fully_invested:
-            break
+        donation = await charityproject_crud.get_oldest_open_donation(session)
+        if donation is None:
+            return project
 
-        need_donation = project.full_amount - project.invested_amount
-        if need_donation <= remaining_donation_amount:
-            project.invested_amount += need_donation
-            invested_amount += need_donation
-            remaining_donation_amount -= need_donation
-        else:
-            project.invested_amount += remaining_donation_amount
-            invested_amount += remaining_donation_amount
-            remaining_donation_amount = 0
+        free_donation = donation.full_amount - donation.invested_amount
 
-        if project.invested_amount >= project.full_amount:
+        if free_donation < need_donation:
+            project.invested_amount += free_donation
+            donation.invested_amount = donation.full_amount
+            donation.fully_invested = True
+            donation.close_date = datetime.now()
+            need_donation -= free_donation
+
+        elif free_donation == need_donation:
+            project.invested_amount = project.full_amount
+            donation.invested_amount = donation.full_amount
+            donation.fully_invested = True
+            donation.close_date = datetime.now()
             project.fully_invested = True
             project.close_date = datetime.now()
+            need_donation = 0
 
-    donation.invested_amount += invested_amount
-    donation.fully_invested = project.fully_invested
-    if project.fully_invested:
-        donation.close_date = datetime.now()
+        elif free_donation > need_donation:
+            project.invested_amount = project.full_amount
+            donation.invested_amount += need_donation
+            project.fully_invested = True
+            project.close_date = datetime.now()
+            need_donation = 0
 
     await session.commit()
-
     return project
